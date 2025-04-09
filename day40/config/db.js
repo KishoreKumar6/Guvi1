b916a0cad6703bd84d134cce9d777a278436d73d
kishorekumar6 <kishoreramu20@gmail.com>
2025-04-09 13:20:27 +0530

    changes

--- day40/config/db.js
+++ day40/config/db.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const connectDB = () => {
     mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("mongodb connected successfully"))
-    .catch((err) => console.log("connection failure"))
+    .catch((err) => console.log("connection failure",err))
 }
 
 module.exports = connectDB
